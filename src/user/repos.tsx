a3672b4510c6ee279378c14a260c79e12e760d25
lexmin0412 <zhangle_media@hotmail.com>
2022-02-20 20:10:05 +0800

    refactor: 修改项目详情跳转参数

--- src/user/repos.tsx
+++ src/user/repos.tsx
@@ -12,8 +12,9 @@ import dayjs from 'dayjs'
 import './repos.scss'
 
 const Repos = (): JSX.Element => {
-
-	const {params: { type, userName }} = useRouter() as any
+	const {
+		params: { type, userName },
+	} = useRouter() as any
 
 	const [repos, setRepos] = useState<Array<any>>([])
 
@@ -22,7 +23,7 @@ const Repos = (): JSX.Element => {
 	const [loading, setLoading] = useState(false)
 	const [scrollTop, setScrollTop] = useState(0)
 
-	usePageScroll(({scrollTop: aScrollTop}) => setScrollTop(aScrollTop))
+	usePageScroll(({ scrollTop: aScrollTop }) => setScrollTop(aScrollTop))
 
 	useEffect(() => {
 		getReposByPage()
@@ -30,32 +31,38 @@ const Repos = (): JSX.Element => {
 
 	const getReposByPage = () => {
 		setLoading(true)
-		usersService.getUserDataByType({
-			userName,
-			type,
-			sort: 'updated',
-			page
-		}).then((res: any) => {
-			console.log('res', res)
-			setRepos([...repos, ...res.data])
-			setPage(page + 1)
-			setLoading(false)
-			if ( res.data && res.data.length < 30 ) {
-				setHasMore(false)
-			}
-		})
+		usersService
+			.getUserDataByType({
+				userName,
+				type,
+				sort: 'updated',
+				page,
+			})
+			.then((res: any) => {
+				console.log('res', res)
+				setRepos([...repos, ...res.data])
+				setPage(page + 1)
+				setLoading(false)
+				if (res.data && res.data.length < 30) {
+					setHasMore(false)
+				}
+			})
 	}
 
-	const handleRepoClick = (userName: string) => {
+	const handleRepoClick = (item: any) => {
+		const {
+			owner: { login },
+			name,
+		} = item
 		Router.navigateTo({
-			url: `/repos/detail?userName=${userName}`
+			url: `/repo/detail?owner=${login}&name=${name}`,
 		})
 	}
 
 	return (
 		<View className='users-repos-page'>
 			<Sticky>
-				<Navbar title="项目" />
+				<Navbar title='项目' />
 			</Sticky>
 			<List
 				loading={loading}
@@ -63,39 +70,32 @@ const Repos = (): JSX.Element => {
 				scrollTop={scrollTop}
 				onLoad={getReposByPage}
 			>
-				{
-					repos.map((item: any) => {
-						return (
-							<View className="repo-item"
-								key={item.id}
-								onClick={() => handleRepoClick(item)}
-							>
-								<View className="repo-item-header">
-									<View className="repo-item-title">
-										{item.name}
-									</View>
-									<View className="repo-item-starrgazer">
-										⭐️ {item.stargazers_count}
-									</View>
-								</View>
-								<View className="repo-item-desc">
-									{item.description}
-								</View>
-								<View className="repo-item-bottom">
-									<View className="repo-item-language">
-										{item.language}
-									</View>
-									<View className="repo-item-update-time">
-										{dayjs(item.updated_at).format('YYYY-MM-DD')}
-									</View>
+				{repos.map((item: any) => {
+					return (
+						<View
+							className='repo-item'
+							key={item.id}
+							onClick={() => handleRepoClick(item)}
+						>
+							<View className='repo-item-header'>
+								<View className='repo-item-title'>{item.name}</View>
+								<View className='repo-item-starrgazer'>
+									⭐️ {item.stargazers_count}
 								</View>
 							</View>
-						)
-					})
-				}
+							<View className='repo-item-desc'>{item.description}</View>
+							<View className='repo-item-bottom'>
+								<View className='repo-item-language'>{item.language}</View>
+								<View className='repo-item-update-time'>
+									{dayjs(item.updated_at).format('YYYY-MM-DD')}
+								</View>
+							</View>
+						</View>
+					)
+				})}
 				<List.Placeholder>
 					{loading && <Loading>加载中...</Loading>}
-					{!hasMore && "没有更多了"}
+					{!hasMore && '没有更多了'}
 				</List.Placeholder>
 			</List>
 		</View>
